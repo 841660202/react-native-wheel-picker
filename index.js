2ad0ed4639f9dc43300ca6ad6f866531c63193bb
昊天 <haotian.chen@tuya.com>
2021-07-08 17:38:26 +0800

    支持rn 0.64.0

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 import React from 'react';
 
 import {
-	PickerIOS,
 	Platform,
 } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+
 
 import WheelCurvedPicker from './WheelCurvedPicker'
 
-module.exports = (Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker)
+module.exports = (Platform.OS === 'ios' ? Picker : WheelCurvedPicker)
